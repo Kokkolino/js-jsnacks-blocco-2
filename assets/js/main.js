51fc9f126a8f595afafa705529ee9a67c09f5c1f
Francesco C <fran461j@outlook.it>
2022-10-25 15:54:42 +0200

    third exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,17 @@
-document.getElementById('inputPair').addEventListener('click', pair);
+document.getElementById('inputPair').addEventListener('click', even);
 document.getElementById('gatsbyParty').addEventListener('click', readInvite);
 document.getElementById('hitmanParty').addEventListener('click', hitmanPartyInvite);
+document.getElementById('oddArray').addEventListener('click', sumOdd);
 
 
 
-function pair(){
-    let pairNumber = parseInt(prompt("immetti un numero"));
-    if (pairNumber % 2 == 0){
-        alert(`${pairNumber} é un numero pari`);
+function even(){
+    let evenNumber = parseInt(prompt("immetti un numero"));
+    if (evenNumber % 2 == 0){
+        alert(`${evenNumber} é un numero pari`);
     }
     else{
-        alert(`${pairNumber + 1} é il numero successivo pari`);
+        alert(`${evenNumber + 1} é il numero successivo pari`);
     }
 }
 
@@ -41,4 +42,19 @@ function readInvite(){
     for (let i = 0; i < guestNumber; ++i){
         alert(`${i + 1} invitato ${nomiInvitati[i]+" "+cognomiInvitati[i]}`);
     }
+}
+
+function sumOdd(){
+    let z = parseInt(prompt("quanti numeri vuoi immettere?"));
+    let sum = 0;
+    let oddArray = [];
+    for (let i = 0; i < z; ++i){
+        oddArray.push(parseInt(prompt("immetti un numero")));
+        if(i % 2 != 0){
+            sum += oddArray[i];
+        }
+    }
+    console.log(oddArray);
+    console.log(sum);
+    alert(`la somma é ${sum}`);
 }
